a64c79ce7203677d84200b716023a5bffdcd6e02
kp1153 <kamta_tiwari@yahoo.com>
2026-05-03 14:37:45 +0530

    force deploy

--- app/api/auth/login/route.js
+++ app/api/auth/login/route.js
@@ -16,14 +16,14 @@ export async function GET() {
     httpOnly: true,
     maxAge: 600,
     path: "/",
-    sameSite: "lax",
+    sameSite: "none",
     secure: true,
   });
   response.cookies.set("code_verifier", codeVerifier, {
     httpOnly: true,
     maxAge: 600,
     path: "/",
-    sameSite: "lax",
+    sameSite: "none",
     secure: true,
   });
   return response;
