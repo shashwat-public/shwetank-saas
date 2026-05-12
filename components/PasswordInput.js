0648c08002d11d05f6c158dea4f729ea3cd40e74
kp1153 <kamta_tiwari@yahoo.com>
2026-05-12 12:56:49 +0530

    force deploy

--- components/PasswordInput.js
+++ components/PasswordInput.js
@@ -1,18 +1,24 @@
 "use client";
 import { useState } from "react";
 
-export default function PasswordInput({ value, onChange }) {
+export default function PasswordInput({
+  name,
+  placeholder,
+  maxLength,
+  minLength,
+  extraClass,
+}) {
   const [showPassword, setShowPassword] = useState(false);
   return (
     <div className="relative">
       <input
-        id="password"
-        name="password"
+        name={name || "password"}
         type={showPassword ? "text" : "password"}
         required
-        value={value}
-        onChange={onChange}
-        className="appearance-none relative block w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 pr-10"
+        placeholder={placeholder || ""}
+        maxLength={maxLength}
+        minLength={minLength}
+        className={`appearance-none relative block w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 pr-10 ${extraClass || ""}`}
       />
       <button
         type="button"
